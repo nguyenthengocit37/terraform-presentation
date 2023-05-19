6dda64153e4f8cce81c0c5b76db7a4f4aa13feee
nguyenthengocit37 <nguyenthengocit37@gmail.com>
2023-05-19 12:04:37 +0700

    refactor(terraform): remove key_name variable from aws_instance resource to avoid hardcoding and use instance profile instead feat(terraform): add variables for region, aws_access_key_id and aws_secret_access_key to allow for dynamic configuration of AWS provider credentials and region.

--- 1.example/variables.tf
+++ 1.example/variables.tf
@@ -2,3 +2,13 @@ variable "cidr_block" {
   type    = string
   default = "172.16.0.0/16"
 }
+
+variable "region" {
+  type = string
+}
+variable "aws_access_key_id" {
+  type = string
+}
+variable "aws_secret_access_key" {
+  type = string
+}
